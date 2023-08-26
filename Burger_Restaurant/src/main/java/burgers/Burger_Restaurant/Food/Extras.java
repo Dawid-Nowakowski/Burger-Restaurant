ccf00049c49263f98f081d3a47fa2daf4daa4a3c
Dawid-Nowakowski <133372396+Dawid-Nowakowski@users.noreply.github.com>
2023-08-26 21:56:29 +0200

    Improved Extras code, added new method and test
    Extras:
    Improved the code, removed the ExtrasManager constructor to prevent a situation where recursive invocation could occur while creating new Extras in availableExtras.
    
    - (new) setSize() - enables the size of current Extras to be changed according to demand, with an associated price adjustment.
    - (new test) shouldSetSize() - verifies the functionality of setSize().

--- Burger_Restaurant/src/main/java/burgers/Burger_Restaurant/Food/Extras.java
+++ Burger_Restaurant/src/main/java/burgers/Burger_Restaurant/Food/Extras.java
@@ -4,37 +4,50 @@ import java.util.List;
 
 public class Extras extends Product {
     public static class ExtrasManager {
-        private final List<Extras> availableExtras;
-
-        public ExtrasManager() {
-            availableExtras = createAvailableExtras();
-        }
+        private static final List<Extras> availableExtras = createAvailableExtras();
 
         private static List<Extras> createAvailableExtras() {
             return List.of(
-                    /*0*/  new Extras("Coke", Extras.Type.DRINK, Extras.Size.SMALL, 2.1),
-                    /*1*/  new Extras("Coke", Extras.Type.DRINK, Extras.Size.MEDIUM, 2.6),
-                    /*2*/  new Extras("Orangeade", Extras.Type.DRINK, Extras.Size.SMALL, 1.7),
-                    /*3*/  new Extras("Orangeade", Extras.Type.DRINK, Extras.Size.MEDIUM, 2.06),
-                    /*4*/  new Extras("Apple Juice", Extras.Type.DRINK, Extras.Size.SMALL, 1.85),
-                    /*5*/  new Extras("Orange Juice", Extras.Type.DRINK, Extras.Size.SMALL, 1.85),
-                    /*6*/  new Extras("Black Tea", Extras.Type.DRINK, Extras.Size.MEDIUM, 2.2),
-                    /*7*/  new Extras("Green Tea", Extras.Type.DRINK, Extras.Size.MEDIUM, 2.32),
-                    /*8*/  new Extras("Black Coffee", Extras.Type.DRINK, Extras.Size.MEDIUM, 2.15),
-                    /*9*/  new Extras("Cappuccino", Extras.Type.DRINK, Extras.Size.MEDIUM, 2.17),
-                    /*10*/ new Extras("Latte", Extras.Type.DRINK, Extras.Size.MEDIUM, 2.3),
-                    /*11*/ new Extras("Fries", Extras.Type.SNACK, Extras.Size.SMALL, 2.7),
-                    /*12*/ new Extras("Fries", Extras.Type.SNACK, Extras.Size.MEDIUM, 3.2),
-                    /*13*/ new Extras("Fries", Extras.Type.SNACK, Extras.Size.LARGE, 3.6),
-                    /*14*/ new Extras("Onion Rings", Extras.Type.SNACK, Extras.Size.SMALL, 2.94),
-                    /*15*/ new Extras("Onion Rings", Extras.Type.SNACK, Extras.Size.MEDIUM, 3.48),
-                    /*16*/ new Extras("Chicken Strips", Extras.Type.SNACK, Extras.Size.SMALL, 3.2),
-                    /*17*/ new Extras("Chicken Strips", Extras.Type.SNACK, Extras.Size.MEDIUM, 3.7),
-                    /*18*/ new Extras("Chicken Strips", Extras.Type.SNACK, Extras.Size.LARGE, 4.2)
+                    /*0*/   new Extras("Coke", Type.DRINK, Size.SMALL, 2.1),
+                    /*1*/   new Extras("Coke", Type.DRINK, Size.MEDIUM, 2.6),
+                    /*2*/   new Extras("Coke", Type.DRINK, Size.LARGE, 3.73),
+                    /*3*/   new Extras("Orangeade", Type.DRINK, Size.SMALL, 1.7),
+                    /*4*/   new Extras("Orangeade", Type.DRINK, Size.MEDIUM, 2.06),
+                    /*5*/   new Extras("Orangeade", Type.DRINK, Size.LARGE, 2.8),
+                    /*6*/   new Extras("Apple Juice", Type.DRINK, Size.SMALL, 1.85),
+                    /*7*/   new Extras("Apple Juice", Type.DRINK, Size.MEDIUM, 2.35),
+                    /*8*/   new Extras("Apple Juice", Type.DRINK, Size.LARGE, 3.21),
+                    /*9*/   new Extras("Orange Juice", Type.DRINK, Size.SMALL, 1.85),
+                    /*10*/  new Extras("Orange Juice", Type.DRINK, Size.MEDIUM, 2.35),
+                    /*11*/  new Extras("Orange Juice", Type.DRINK, Size.LARGE, 3.09),
+                    /*12*/  new Extras("Black Tea", Type.DRINK, Size.SMALL, 1.76),
+                    /*13*/  new Extras("Black Tea", Type.DRINK, Size.MEDIUM, 2.2),
+                    /*14*/  new Extras("Black Tea", Type.DRINK, Size.LARGE, 2.7),
+                    /*15*/  new Extras("Green Tea", Type.DRINK, Size.SMALL, 2.02),
+                    /*16*/  new Extras("Green Tea", Type.DRINK, Size.MEDIUM, 2.32),
+                    /*17*/  new Extras("Green Tea", Type.DRINK, Size.LARGE, 3.16),
+                    /*18*/  new Extras("Black Coffee", Type.DRINK, Size.SMALL, 1.90),
+                    /*19*/  new Extras("Black Coffee", Type.DRINK, Size.MEDIUM, 2.15),
+                    /*20*/  new Extras("Black Coffee", Type.DRINK, Size.LARGE, 2.87),
+                    /*21*/  new Extras("Cappuccino", Type.DRINK, Size.SMALL, 1.99),
+                    /*22*/  new Extras("Cappuccino", Type.DRINK, Size.MEDIUM, 2.17),
+                    /*23*/  new Extras("Cappuccino", Type.DRINK, Size.LARGE, 2.98),
+                    /*24*/  new Extras("Latte", Type.DRINK, Size.SMALL, 1.95),
+                    /*25*/  new Extras("Latte", Type.DRINK, Size.MEDIUM, 2.3),
+                    /*26*/  new Extras("Latte", Type.DRINK, Size.LARGE, 2.79),
+                    /*27*/  new Extras("Fries", Type.SNACK, Size.SMALL, 2.7),
+                    /*28*/  new Extras("Fries", Type.SNACK, Size.MEDIUM, 3.2),
+                    /*29*/  new Extras("Fries", Type.SNACK, Size.LARGE, 3.6),
+                    /*30*/  new Extras("Onion Rings", Type.SNACK, Size.SMALL, 2.94),
+                    /*31*/  new Extras("Onion Rings", Type.SNACK, Size.MEDIUM, 3.48),
+                    /*32*/  new Extras("Onion Rings", Type.SNACK, Size.LARGE, 4.18),
+                    /*33*/  new Extras("Chicken Strips", Type.SNACK, Size.SMALL, 3.2),
+                    /*34*/  new Extras("Chicken Strips", Type.SNACK, Size.MEDIUM, 3.7),
+                    /*35*/  new Extras("Chicken Strips", Type.SNACK, Size.LARGE, 4.2)
             );
         }
 
-        public List<Extras> getAvailableExtras() {
+        public static List<Extras> getAvailableExtras() {
             return availableExtras;
         }
     }
@@ -64,8 +77,10 @@ public class Extras extends Product {
             return null;
         }
     }
+
     private final Type type;
-    private final Size size;
+    private Size size;
+    private final List<Extras> extrasList = new ExtrasManager().getAvailableExtras();
 
     private Extras(String name, Type type, Size size, Double price) {
         super(name + " " + size.getValue(type));
@@ -76,10 +91,9 @@ public class Extras extends Product {
 
     public Extras(int index) {
         super("Extra");
-        ExtrasManager extrasManager = new ExtrasManager();
         Extras extras = null;
         try {
-            extras = extrasManager.getAvailableExtras().get(index);
+            extras = extrasList.get(index);
         } catch (IndexOutOfBoundsException ioobe) {
             System.out.println("Exception: Unauthorised extra");
         }
@@ -99,6 +113,19 @@ public class Extras extends Product {
 
     public Size getSize() {
         return size;
+    }
+
+    public void setSize(Size size) {
+        Size actual = this.getSize();
+        if (actual != size) {
+            for (var e : extrasList) {
+                if(getName().substring(0,getName().length() - 5).equals(e.getName().substring(0,e.getName().length() - 5)) && e.getSize() == size){
+                    this.setPrice(e.getPrice());
+                    this.size = e.getSize();
+                    break;
+                }
+            }
+        }
     }
 
     @Override
--- Burger_Restaurant/src/test/java/burgers/Burger_Restaurant/Food/ExtrasTest.java
+++ Burger_Restaurant/src/test/java/burgers/Burger_Restaurant/Food/ExtrasTest.java
@@ -6,8 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ExtrasTest {
 
-    private final Extras.ExtrasManager extrasManager = new Extras.ExtrasManager();
-
     @Test
     public void testValidExtra() {
         Extras extras = new Extras(0);
@@ -24,11 +22,22 @@ public class ExtrasTest {
 
     @Test
     public void shouldGetExtra() {
-        assertDoesNotThrow(() -> extrasManager.getAvailableExtras().get(0));
+        assertDoesNotThrow(() -> Extras.ExtrasManager.getAvailableExtras().get(0));
     }
 
     @Test
     public void shouldNotGetExtra() {
-        assertThrows(IndexOutOfBoundsException.class, () -> extrasManager.getAvailableExtras().get(50));
+        assertThrows(IndexOutOfBoundsException.class, () -> Extras.ExtrasManager.getAvailableExtras().get(50));
+    }
+
+    @Test
+    public void shouldSetSize(){
+        Extras extras = new Extras(0);
+        var extrasOldSize = extras.getSize();
+        var extrasOldPrice = extras.getPrice();
+        extras.setSize(Extras.Size.LARGE);
+
+        assertNotEquals(extrasOldPrice, extras.getPrice());
+        assertNotEquals(extrasOldSize, extras.getSize());
     }
 }
